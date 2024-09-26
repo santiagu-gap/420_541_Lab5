775a50f4808668894864e408ed2c40b70fe9d3f8
Santiago Garcia <santiagu.gap@gmail.com>
2024-09-26 15:20:58 -0400

    Lab 5 is done!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -78,11 +78,12 @@ public class PlayerMovement : MonoBehaviour
     private void OnAnimatorMove()
     {
         // If root motion is enabled, apply the animation's motion directly to the controller
-       
-        Vector3 velocity = animator.deltaPosition;
-        velocity.y = playerVelocity.y * Time.deltaTime; // Preserve vertical movement (gravity, jump)
-        controller.Move(velocity);
-        
+        if (animator.applyRootMotion)
+        {
+            Vector3 velocity = animator.deltaPosition;
+            velocity.y = playerVelocity.y * Time.deltaTime; // Preserve vertical movement (gravity, jump)
+            controller.Move(velocity);
+        }
     }
 
     public float GetMoveSpeed()
